d8fbe671426bfcfe9199f450b901f82b15f0cbfc
volodymyrkr <volodymyr.kravchenko.2000@gmail.com>
2019-11-14 02:47:03 +0200

    Simple Matrix renderer

--- src/components/cell/cell.component.tsx
+++ src/components/cell/cell.component.tsx
@@ -2,12 +2,32 @@ import * as React from "react";
 
 import {FC, Props} from "react";
 
+import "./index.css";
+
 type CellProps =  {
   color?: string;
 }
+
+const data: ReadonlyArray<ReadonlyArray<number>> = [
+  [1,0,1,0,0],
+  [0,1,0,1,0],
+  [0,1,0,1,1],
+  [0,0,0,1,1],
+  [1,1,0,1,1],
+]
+
 export const Cell: FC<CellProps> = (props: CellProps) => (
-  <div className="cell">`${props.color}`</div>
-)
+  <div className="col">
+    {
+      data.map((row: ReadonlyArray<number>, indexRow)=> (
+        <div key={indexRow} className="row">
+          { row.map((value: number, indexCol) => (<div key={indexCol+indexRow*3} className="cell">{value}</div>)) }
+          <br/>
+        </div>
+      ))
+    }
+  </div>
+);
 
 Cell.defaultProps = {
   color: "000000"
